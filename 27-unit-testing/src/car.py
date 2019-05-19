48fe12ee457ae3718a149c823e39a4eaf53c29e1
ynonp <ynonperek@gmail.com>
2019-05-19 21:10:23 +0300

    add gitignore and fixed style

--- 27-unit-testing/src/car.py
+++ 27-unit-testing/src/car.py
@@ -7,7 +7,7 @@ class Car:
         print(f"{self.color} car, speed = {self.speed}")
 
     def is_faster_than(self, other_car):
-        return True if self.speed > other_car.speed else False
+        return self.speed > other_car.speed
 
 
 hello = 10
